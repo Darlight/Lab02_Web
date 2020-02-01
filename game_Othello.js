7bcd98441081c25730fb5fc045ee622c44c07550
Darlight <andres.perdomo09@gmail.com>
2020-02-01 17:42:18 -0600

    Creating the initial code to start the project.

---
+++ game_Othello.js
@@ -0,0 +1,13 @@
+/*
+|---------------------------------------|
+Universidad del Valle de Guatemala
+Seccion 10	Sistemas y web
+Ing. Samuel Chavez
+Mario Perdomo
+Carné 18029
+game_Othello.js
+|---------------------------------------|
+*/
+const map = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+const player1_Points = 0;
+const player2_Points = 0;
