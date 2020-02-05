de96ae0133ac2fa99911e50de959402d30daf784
Darlight <andres.perdomo09@gmail.com>
2020-02-05 09:19:28 -0600

    Advancing

--- game_Othello.js
+++ game_Othello.js
@@ -8,6 +8,49 @@ Carné 18029
 game_Othello.js
 |---------------------------------------|
 */
-const map = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+const root = document.getElementById('root');
+const map_state = {
+	player_Turn: false,
+	mapboard:[
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,1,-1,0,0,0],
+	[0,0,0,-1,1,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0]
+	]
+};
+
+const render = (root, state) =>{
+	const header = document.createElement('div');
+	header.style{
+		
+	}
+}
 const player1_Points = 0;
 const player2_Points = 0;
+const player_Turn = false;
+
+// player_Turn
+
+
+/*
+|---------Javascript-------------|
+  boton.onclick = () => {
+        state.turnedOnIndex = (state.turnedOnIndex + 1) % 3;
+        root.innerHTML = '';
+        render(root, state);
+    };
+
+
+const APP_STATE = {
+    turnedOnIndex: 1,
+};
+
+|---------HTML-------------|
+
+    mount.appendChild(semaforo);
+    mount.appendChild(boton);
+*/
